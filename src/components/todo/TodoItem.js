d9cb939d0b2227bc1c8fa1d14f78d802859d9c6e
Nicolas Sabaj <nsabaj@hotmail.com>
2020-03-01 20:31:18 -0300

    Added partial func

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {partial} from '../../lib/utils'
 
 export const TodoItem = (props) => 
-    <li>
-        <input type="checkbox" onChange={() => {props.handleToggle(props.id)}} checked={props.isComplete}/> 
-        {props.name}
-    </li>
+{
+    const handleToggle = partial(props.handleToggle, props.id)
+    return (
+        <li>
+            <input type="checkbox" onChange={handleToggle} checked={props.isComplete}/> 
+            {props.name}
+        </li>
+    )
+}
 
 TodoItem.propTypes = {
     name: PropTypes.string.isRequired,
